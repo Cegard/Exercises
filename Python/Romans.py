21e689c73344f016c1ee712208db735cb5410028
Eduardo Galeano <cegard689@gmail.com>
2015-07-29 19:36:01 -0500

    Lista de decimales y representación romana
    Se creó una lista de tuplas que contienen los números decimales y su
    respectiva representación en romanos.

--- Python/Romans.py
+++ Python/Romans.py
@@ -1,39 +1,28 @@
 __author__ = "Eduardo Galeano"
 
 def parse_to_roman(n):
+    """gives the roman representation of the given number
+    :param n: the decimal number to be parsed
+    :returns: roman representation"""
+    
+    # lista de tuplas cada una contiene la cifra decimal y su respectiva representación en romanos
+    numbers = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'),
+        (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (4, 'I')]
+    
     number = ''
     
     def _add_substract(unit, char):
+        """adds the chars to the roman representation
+        while substracts to the number.
+        :param chars: the chars to be added
+        :param unit: the units to be substracted"""
+        
         nonlocal number, n
         
         number += char*(n//unit) if n >= unit else ''
         n -= unit*(n//unit)
     
-    _add_substract(1000, 'M')
-    
-    _add_substract(900, 'CM')
-    
-    _add_substract(500, 'D')
-    
-    _add_substract(400, 'CD')
-    
-    _add_substract(100, 'C')
-    
-    _add_substract(90, 'XC')
-    
-    _add_substract(50, 'L')
-    
-    _add_substract(40, 'XL')
-    
-    _add_substract(10, 'X')
-    
-    _add_substract(9, 'IX')
-    
-    _add_substract(5, 'V')
-    
-    _add_substract(4, 'IV')
-    
-    _add_substract(1, 'I')
+    for t in numbers: _add_substract(t[0],t[1])
     
     return number
 
