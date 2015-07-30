bceebc99d90f789e82705dcf637855c4fe288c95
Eduardo Galeano <cegard689@gmail.com>
2015-07-29 20:02:51 -0500

    Versión final
    La función se le cambió el nombre a la función parse_to _roman por
    _parse se convirtió en una función anidada, para hacerla más general. Se
    añadió la parte de lectura de teclado y de validación del número; el
    número no puede ser mayor a 4000 debido a la dificultad de este número
    para ser mostrado en la consola, y debe ser mayor a cero debido a que no
    hay una representación en romano para este número o menores

--- Python/Romans.py
+++ Python/Romans.py
@@ -1,41 +1,48 @@
 __author__ = "Eduardo Galeano"
 
-def parse_to_roman(n):
+
+def parse_to_roman(m):
     """gives the roman representation of the given number
-    :param n: the decimal number to be parsed
+    :param m: the decimal number to be parsed
     :returns: roman representation"""
     
     # lista de tuplas cada una contiene la cifra decimal y su respectiva representación en romanos
     numbers = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'),
-        (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (4, 'I')]
+        (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
     
-    number = ''
-    
-    def _add_substract(unit, char):
-        """adds the chars to the roman representation
-        while substracts to the number.
-        :param chars: the chars to be added
-        :param unit: the units to be substracted"""
+    def _parse(n, tuples):
+        """gives the representation of the given number, according to the given list
+        :param n: the decimal number to be parsed
+        :returns: the number parsed"""
         
-        nonlocal number, n
+        number = ''
         
-        number += char*(n//unit) if n >= unit else ''
-        n -= unit*(n//unit)
+        def _add_substract(unit, char):
+            """adds the chars to the representation
+            while substracts to the number.
+            :param chars: the chars to be added
+            :param unit: the units to be substracted"""
+            
+            nonlocal number, n
+            
+            number += char*(n//unit) if n >= unit else ''
+            n -= unit*(n//unit)
+        
+        for t in tuples: _add_substract(t[0],t[1])
+        
+        return number
     
-    for t in numbers: _add_substract(t[0],t[1])
-    
-    return number
+    return _parse(m, numbers)
 
-#number = input()
 
-n0 = 8
-n1 = 991
-n2 = 1523
-n3 = 1024
-n4 = 3589
+err_msg = 'número ingeresado no valido'
+print("por favor digite el número a pasar a romanos que no sea mayor a 4000")
 
-print(n0, ' en romanos es: ', parse_to_roman(n0))
-print(n1, ' en romanos es: ', parse_to_roman(n1))
-print(n2, ' en romanos es: ', parse_to_roman(n2))
-print(n3, ' en romanos es: ', parse_to_roman(n3))
-print(n4, ' en romanos es: ', parse_to_roman(n4))
+number = input()
+
+try:
+    number = int(number)
+    if number < 4000 and number > 0: print(number, ' en romanos es: ', parse_to_roman(number))
+    else: print(err_msg)
+except:
+    print(err_msg)
