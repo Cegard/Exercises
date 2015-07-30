5c2136aae9a9b0e3aa8a58aa58f212fe580fe68d
Eduardo Galeano <cegard689@gmail.com>
2015-07-29 19:10:03 -0500

    Enfoque cambiado
    Dado que el enfoque usado antes demandaba un "alto" costo computacional
    se decidió cambiar el enfoque, en vez de usar iteraciones, se usa
    división entera.

--- Python/Romans.py
+++ Python/Romans.py
@@ -1,5 +1,5 @@
 __author__ = "Eduardo Galeano"
-
+'''
 def parse_to_roman(n):
     """gives the roman representation of the given number
     :param n: the decimal number to be parsed
@@ -45,6 +45,52 @@ def parse_to_roman(n):
     add_substract('IV', 4)
     
     add_substract('I', 1)
+    
+    
+    return number
+'''
+
+def parse_to_roman(n):
+    number = ''
+    
+    number += 'M'*(n//1000) if n >= 1000 else ''
+    n -= 1000*(n//1000)
+    
+    number += 'CM'*(n//900) if n >= 900 else ''
+    n -= 900*(n//900)
+    
+    number += 'D'*(n//500) if n >= 500 else ''
+    n -= 500*(n//500)
+    
+    number += 'CD'*(n//400) if n >= 400 else ''
+    n -= 400*(n//400)
+    
+    number += 'C'*(n//100) if n >= 100 else ''
+    n -= 100*(n//100)
+    
+    number += 'XC'*(n//90) if n >= 90 else ''
+    n -= 90*(n//90)
+    
+    number += 'L'*(n//50) if n >= 50 else ''
+    n -= 50*(n//50)
+    
+    number += 'XL'*(n//40) if n >= 40 else ''
+    n -= 40*(n//40)
+    
+    number += 'X'*(n//10) if n >= 10 else ''
+    n -= 10*(n//10)
+    
+    number += 'IX'*(n//9) if n >= 9 else ''
+    n -= 9*(n//9)
+    
+    number += 'V'*(n//5) if n >= 5 else ''
+    n -= 5*(n//5)
+    
+    number += 'IV'*(n//4) if n >= 4 else ''
+    n -= 4*(n//4)
+    
+    number += 'I'*(n//1) if n >= 1 else ''
+    n -= 1*(n//1)
     
     return number
 
